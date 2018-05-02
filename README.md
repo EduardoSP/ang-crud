8e24cdb863b20dec33f46e8f00dddfca38e38fb6
eduardo <eduardo.saavedra@correounivalle.edu.co>
2018-05-02 16:01:57 -0500

    add files save and edit data

--- README.md
+++ README.md
@@ -26,3 +26,7 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
 "# ang-crud" 
+
+Note:
+1) import on the file app.module.ts in the package FormsModule
+2) For install bootstrap writes the comand npm i bootstrap --save and write on .angular-cli.json file, in the line of "styles" the code ../node_modules/bootstrap/dist/css/bootstrap.min.css
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title : string = 'Angular crud';
+  //Array of employees
+  employees =[
+    {'name': 'Eduardo', position: 'Manager', email:'edu@gmail.com'},
+    {'name': 'Joan', position: 'Designer', email:'joan@gmail.com'},
+    {'name': 'Isa', position: 'Programmer', email:'isa@gmail.com'}
+  ];
+
+  model:any = {}; // model of type "any" will be any array
+  model2:any = {};
+  addEmployee():void{
+    this.employees.push(this.model);
+
+  }
+  deleteEmployeed():void{
+
+  }
+
+  myValue;//this variable will contain the index of the value to be edit
+  editEmployeed(i):void{
+    this.model2.name      = this.employees[i].name;
+    this.model2.position  = this.employees[i].position;
+    this.model2.email     = this.employees[i].email;
+    this.myValue = i;
+    
+  }
+
+  updateEmployeed():void{
+
+  }
 }
